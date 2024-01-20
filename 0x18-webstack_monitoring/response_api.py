debe893eab5ac01170a391f5e53d503c27980e80
yos <yosef.debela@ju.edu.et>
2024-01-20 15:52:42 +0300

    check

--- 0x18-webstack_monitoring/response_api.py
+++ 0x18-webstack_monitoring/response_api.py
@@ -1,5 +1,12 @@
+import os
+from dotenv import load_dotenv
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v1.api.hosts_api import HostsApi
+load_dotenv()
+# Store credentials
+pwd = os.getenv('DD_APP_KEY')
+key = os.getenv('DD_API_KEY')
+
 
 configuration = Configuration()
 with ApiClient(configuration) as api_client:
