11c84b30a9f526f9e344b969af4e3f272f4c8d7e
yos <yosef.debela@ju.edu.et>
2024-05-08 22:34:20 +0300

    reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given subreddit
+number of subscribers for a given Subreddit
 """
 import requests
 
@@ -8,17 +8,17 @@ import requests
 # from requests import get
 
 
-def number_of_subscribers(subreddit):
+def number_of_subscribers(Subreddit):
     """
     function that queries the    Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    (not active users, total subscribers) for a given Subreddit.
     """
-    if subreddit is None or not isinstance(subreddit, str):
+    if Subreddit is None or not isinstance(Subreddit, str):
         return 0
 
     user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
                                 '(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(Subreddit)
     response = requests.get(url, headers=user_agent)
     results = response.json()
 
