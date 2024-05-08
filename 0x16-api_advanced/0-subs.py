a3c48a697105044dd338b2352c4bb9da35099d00
yos <yosef.debela@ju.edu.et>
2024-05-08 22:47:40 +0300

    reddit2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
 """
-number of subscribers for a given Subreddit
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
 
-# from requests import get
-
-
-def number_of_subscribers(Subreddit):
-    """
-    function that queries the    Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given Subreddit.
-    """
-    if Subreddit is None or not isinstance(Subreddit, str):
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-    user_agent = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-                                '(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(Subreddit)
-    response = requests.get(url)
-    results = response.json()
-
-    # print(response.content)
-    # print(response.encoding.)
-    # print(results.get("data").get("subscribers"))
-
-    return results.get("data").get("subscribers")
-
-
-print(number_of_subscribers("learnpython"))
-
