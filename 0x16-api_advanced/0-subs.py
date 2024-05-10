78e321788d5e779f59c1a36921732ebecd67d80a
yos <yosef.debela@ju.edu.et>
2024-05-10 23:15:16 +0300

    new subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=True)
-    # print(response.json())
-    if response.status_code == 200:
-        data = response.json()
-        sub = data["data"]["children"][0]["data"]["subscribers"]
-        return sub
-    else:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    
+    results = response.json().get("data")
+    return results.get("subscribers")
