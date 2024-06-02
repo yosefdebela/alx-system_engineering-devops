49c28eaa5881385ce1a7d5808c5312cc8ac34ffd
yos <yosef.debela@ju.edu.et>
2024-06-02 18:35:52 +0300

    None added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,13 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
+    elif response.status_code == 404:
+        return None
+
     else:
         return 0
 
