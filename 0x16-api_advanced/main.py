ee404f9507518e14db5f58690a0f1737a7541961
yos <yosef.debela@ju.edu.et>
2024-06-02 20:04:04 +0300

    recurse

--- 0x16-api_advanced/main.py
+++ 0x16-api_advanced/main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-0-main
+2-main
 """
 import sys
 
 if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    recurse = __import__('2-recurse').recurse
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{}".format(number_of_subscribers(sys.argv[1])))
+        result = recurse(sys.argv[1])
+        if result is not None:
+            print(len(result))
+            # print(result)
+        else:
+            print("None")
