40992908ef87405160d8cb22c6f5fef7edfcf39a
yos <yosef.debela@ju.edu.et>
2024-06-04 15:14:06 +0300

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# fix spelling problem
+5# fix spelling problem with php
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
