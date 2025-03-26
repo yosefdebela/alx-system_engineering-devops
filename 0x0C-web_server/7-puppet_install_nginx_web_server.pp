3304c77c9f068da60314fc226d6ae7ea04fa80c8
yosefdebela <yosef.debela@ju.edu.et>
2025-03-26 04:11:20 +0300

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,38 +1,33 @@
-class nginx_setup {
+# Nginx configuration with Puppet
+class { 'nginx': }
 
-  # Ensure Nginx is installed
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Ensure Nginx service is enabled and running
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
-  }
-
-  # Create the index.html file with "Hello World!"
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0644',
-    require => Package['nginx'],
-  }
-
-  # Configure Nginx default site
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx/default.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
+# Remove default Nginx configuration
+nginx::resource::server { 'default':
+  ensure => absent,
 }
 
-# Apply the nginx_setup class
-include nginx_setup
+# Create custom index.html with Hello World!
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  mode    => '0644',
+}
+
+# Configure Nginx server with redirect
+nginx::resource::server { 'hello_world':
+  ensure               => present,
+  listen_port          => 80,
+  server_name         => ['_'],
+  www_root             => '/var/www/html',
+  index_files          => ['index.html'],
+  location_cfg_append  => {
+    'rewrite' => '^/redirect_me https://www.example.com permanent',
+  },
+}
+
+# Ensure Nginx service is running
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Class['nginx'],
+}
